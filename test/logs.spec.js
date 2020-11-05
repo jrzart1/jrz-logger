936c725ff6df432bb5b9acf51cfed7ed4edf5f57
Jonas Zart <jrzart@gmail.com>
2020-11-05 11:07:01 -0300

    added some tests

--- test/logs.spec.js
+++ test/logs.spec.js
@@ -1,13 +1,24 @@
 const assert = require('assert');
-// const sinon = require('sinon');
-
 const logger = require('../index');
 
-process.env.NODE_ENV = 'Development';
-process.env.LOG_LEVEL = 'trace';
+describe('@ Just call some logs to see format and colors', () => {
+  it('ENV: Development; LEVEL: trace', () => {
+		process.env.NODE_ENV = 'Development';
+		process.env.LOG_LEVEL = 'trace';
+    const log = logger.makeLogger('123456', process.env.LOG_LEVEL);
+    log.fatal('log fatal level');
+    log.error('log error level');
+    log.warn('log warn level');
+    log.success('log success level');
+    log.info('log info level');
+    log.profile('log profile level');
+    log.debug('log debug level');
+    log.trace('log trace level');
+  });
 
-describe('A logger', () => {
-  it('call some logs to see format and colors', () => {
+  it('ENV: Production; LEVEL: trace', () => {
+		process.env.NODE_ENV = 'Production';
+		process.env.LOG_LEVEL = 'trace';
     const log = logger.makeLogger('123456', process.env.LOG_LEVEL);
     log.fatal('log fatal level');
     log.error('log error level');
